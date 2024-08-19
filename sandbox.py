b3992ae73f50f1f0922c10a9c85714cc166282af
Tensor <narutokelechi@gmail.com>
2024-08-19 05:13:33 +0100

    [remove former jax training code] add image dataloader

---
+++ sandbox.py
@@ -0,0 +1,10 @@
+# simple jax intro code
+import jax
+import jax.numpy as jnp
+import numpy as np
+from flax import nnx  # nnx api for model definition
+
+
+def simple_matmul
+
+# jax model for TPU usage
--- train.py
+++
@@ -1,6 +0,0 @@
-import flax
-import jax
-import numpy as np
-import jax.numpy as jnp
-import optax
-from flax.training import train_state
