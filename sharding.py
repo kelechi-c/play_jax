86bc51c999b07c8cc11d182353c52b815dc014c5
Tensor <narutokelechi@gmail.com>
2024-11-20 12:30:43 +0100

    [add] sharding sample script

---
+++ sharding.py
@@ -0,0 +1,20 @@
+import jax
+from jax.sharding import NamedSharding, Mesh, PartitionSpec as PS
+from jax.experimental import mesh_utils
+from jax import random as jrand
+
+device_count = jax.device_count() # 8 devices for tpuv2-8
+devices = jax.devices()
+
+randtensor = jrand.normal(jrand.key(33), (32, 64, 1024))
+
+mesh_devices = mesh_utils.create_device_mesh((device_count,))
+mesh = Mesh(mesh_devices, axis_names="axis")
+
+sharding = NamedSharding(mesh, PS("axis"))
+
+sharded_batch = jax.device_put(randtensor, sharding) # ransfer to device
+
+print(f"original batch shape => {randtensor.shape}")  # (32, 64, 1024)
+print(f'Sharded shape per device => {sharding.shard_shape(sharded_batch.shape)}')
+# (4, 64, 1024) (4x8 gives the orginal batch size of 32 :)
