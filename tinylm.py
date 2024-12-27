47cec435a612008553bdf07b89998699b38f813b
Tensor <narutokelechi@gmail.com>
2024-12-27 17:53:40 +0100

    [gpt model] trainer, loss fxn, main script

--- tinylm.py
+++ tinylm.py
@@ -1,8 +1,6 @@
-from torch import bfloat16
-import jax, math, optax, wandb
-from jax import numpy as jnp, random as jrand
+import jax, math, optax, wandb, click, time, gc
+from jax import numpy as jnp, random as jrand, Array
 from flax import nnx
-from flax.training import checkpoints
 from einops import rearrange
 from tqdm.auto import tqdm
 from datasets import load_dataset
@@ -19,6 +17,8 @@ class config:
     key = jrand.key(seed)
     split = 10000
     learn_rate = 1e-4
+
+randkey = config.key
 
 # confirm devices
 print(f"JAX devices: {jax.local_devices()}")
@@ -66,13 +66,6 @@ def jax_collate(batch):
     }
 
 
-textdata = TextData()
-train_loader = DataLoader(textdata, batch_size=16, collate_fn=jax_collate)
-
-vs = next(iter(train_loader))
-print(f"sample shape: {vs['input_ids'].shape}")
-
-
 xavier_init = nnx.initializers.xavier_uniform()
 zero_init = nnx.initializers.constant(0.0)
 normal_init = nnx.initializers.normal(0.02)
@@ -82,8 +75,17 @@ class MLP(nnx.Module):
     def __init__(self, embed_dim, rngs: nnx.Rngs):
         super().__init__()
         self.layernorm = nnx.LayerNorm(embed_dim, rngs=rngs)
-        self.linear1 = nnx.Linear(embed_dim, 2 * embed_dim, rngs=rngs)
-        self.linear2 = nnx.Linear(2 * embed_dim, embed_dim, rngs=rngs)
+        self.linear1 = nnx.Linear(
+            embed_dim, 2 * embed_dim, rngs=rngs, 
+            kernel_init=xavier_init, bias_init=zero_init
+        )
+        self.linear2 = nnx.Linear(
+            2 * embed_dim,
+            embed_dim,
+            rngs=rngs,
+            kernel_init=xavier_init,
+            bias_init=zero_init,
+        )
 
     def __call__(self, x_input: jax.Array) -> jax.Array:
         x = self.layernorm(x_input)
@@ -94,18 +96,21 @@ class MLP(nnx.Module):
 
 
 class DecoderBlock(nnx.Module):
-    def __init__(self, rngs: nnx.Rngs, embed_dim=embed_dim):
+    def __init__(self, rngs: nnx.Rngs, embed_dim=512, attn_heads=8):
         super().__init__()
         self.layernorm = nnx.LayerNorm(embed_dim, rngs=rngs)
-        self.attention = nnx.MultiHeadAttention(num_heads=attn_heads, in_features=embed_dim, dtype=jnp.bfloat16, decode=True, rngs=rngs)
+        self.attention = nnx.MultiHeadAttention(
+            num_heads=attn_heads, 
+            in_features=embed_dim, 
+            dtype=jnp.bfloat16,
+            decode=True, rngs=rngs
+        )
         self.ffn_layer = MLP(embed_dim, rngs=rngs)
 
-    def __call__(self, x_token: jax.Array) -> jax.Array:
+    def __call__(self, x_token: Array) -> Array:
         x = x_token + self.layernorm(self.attention(x_token))
         x = x + self.layernorm(self.ffn_layer(x))
-
         return x
-
 
 # autoregressive transformer block, or just an SLM
 class Transformer(nnx.Module):
@@ -120,10 +125,18 @@ class Transformer(nnx.Module):
         self.pos_embed = nnx.Embed(
             hidden_size, embed_dim, rngs=rngs, embedding_init=embed_init
         )
-        self.layernorm = nnx.LayerNorm(embed_dim, rngs=rngs)
-        decoder_layers = [DecoderBlock(rngs=rngs) for _ in range(n_layers)]
-        self.decoder_layers = nnx.Sequential(*decoder_layers)
-        self.lm_head = nnx.Linear(embed_dim, vocab_size, rngs=rngs)
+
+        self.layernorm = nnx.LayerNorm(embed_dim, rngs=rngs, epsilon=1e-6)
+
+        self.decoder_layers = [DecoderBlock(rngs=rngs) for _ in range(n_layers)]
+        # self.decoder_layers = nnx.Sequential(*decoder_layers)
+        self.linear_head = nnx.Linear(
+            embed_dim,
+            vocab_size,
+            rngs=rngs,
+            kernel_init=xavier_init,
+            bias_init=zero_init,
+        )
 
     def __call__(self, x_tokens: jax.Array) -> jax.Array:
         b_size, token_len = x_tokens.shape
@@ -133,48 +146,37 @@ class Transformer(nnx.Module):
 
         x = token_embed + pos_embed
 
-        x = self.layernorm(self.decoder_layers(x_tokens))
+        for block in self.decoder_layers:
+            x = block(x)
 
-        x = self.lm_head(x)
-        output = nnx.softmax(x, axis=1)
+        x = self.layernorm(x)
 
-        print(f"lm/softmax out shape => {output.shape}")
+        x = self.linear_head(x)
+        # output = nnx.softmax(x, axis=1)
+        print(f"lm out shape => {x.shape}")
 
-        return output
+        return x
 
-    def generate(self, cond_token, max_outlen=256, temperature=0.1):
+    def generate(self, cond_token, max_outlen=128, temperature=1):
         pass
 
 
-slm_model = Transformer(n_layers, embed_dim, rngs=nnx.Rngs(3))
-# nnx.display(slm_model)
-s = slm_model(jnp.ones((1, 20, 768)))
-print(s.shape)
-
-
-graph, state = nnx.split(slm_model)
-n_params = sum([p.size for p in jax.tree.leaves(state)])
-print(f"number of parameters: {n_params/1e6:.3f}M")
-
-optimizer = nnx.Optimizer(slm_model, optax.adamw(learning_rate=config.learn_rate))
-
-
-def wandb_logger(key: str, project_name, run_name):  # wandb logger
+def wandb_logger(key: str, project_name, run_name=None):  # wandb logger
     # initilaize wandb
     wandb.login(key=key)
-    wandb.init(project=project_name, name=run_name)
+    wandb.init(project=project_name, name=run_name or None)
 
 
 def modelpass(logits, tokens):
-
+    
     output = logits[..., :-1, :]
     targets = tokens[..., 1:].squeeze(1)
 
     output = output.reshape(-1, output.shape[-1])
     targets = targets.reshape(-1)
 
-    output = output.astype(jnp.float32)
-    targets = targets.astype(jnp.b)
+    output = output.astype(jnp.bfloat16)
+    targets = targets.astype(jnp.bfloat16)
 
     return output, targets
 
@@ -202,28 +204,105 @@ def train_step(model, optimizer, batch):
     return loss
 
 
-def trainer(model, optimizer, train_loader):
-    epochs = 1
+def batch_trainer(epochs, model, optimizer, train_loader):
     train_loss = 0.0
     model.train()
-    # wandb_logger(
-    #     key=None,
-    #     model=model,
-    #     project_name="transformer_playjax",
-    #     run_name="tinygpt-1e-4-bs32-tpu",
-    # )
+
+    wandb_logger(key="", project_name="tinylm_jax")
+
+    stime = time.time()
+
+    batch = next(iter(train_loader))
+    print("start overfitting.../")
 
     for epoch in tqdm(range(epochs)):
-        for step, batch in tqdm(enumerate(train_loader), total=len(train_loader)):
+        train_loss = train_step(model, optimizer, batch)
+        print(f"epoch {epoch+1}/{epochs}, train loss => {train_loss.item():.4f}")
+        wandb.log({"loss": train_loss, "log_loss": math.log10(train_loss)})
 
+        if epoch % 50 == 0:
+            pass
+
+        jax.clear_caches()
+        jax.clear_backends()
+        gc.collect()
+
+    etime = time.time() - stime
+    print(
+        f"overfit time for {epochs} epochs -> {etime/60:.4f} mins / {etime/60/60:.4f} hrs"
+    )
+
+
+    return model, train_loss
+
+
+def trainer(epochs, model, optimizer, train_loader):
+    train_loss = 0.0
+    model.train()
+
+    wandb_logger(key="", project_name="tinylm_jax")
+
+    stime = time.time()
+
+    print("start overfitting.../")
+
+    for epoch in tqdm(range(epochs)):
+        for step, batch in tqdm(enumerate(train_loader)):
             train_loss = train_step(model, optimizer, batch)
-            print(f"step {step}, loss-> {train_loss.item():.4f}")
+            print(f"epoch {epoch+1}/{epochs}, train loss => {train_loss:.4f}")
+            wandb.log({"loss": train_loss, "log_loss": math.log10(train_loss)})
 
-            # wandb.log({"loss": train_loss.item()})
+            if epoch % 200 == 0:
+                pass
+            
+            jax.clear_caches()
+            jax.clear_backends()
+            gc.collect()
 
-        print(f"epoch {epoch+1}, train loss => {train_loss}")
+    etime = time.time() - stime
+    print(f"train time for {epochs} epochs -> {etime/60/60:.4f} hrs")
 
 
-trainer(slm_model, optimizer, train_loader)
-wandb.finish()
-print("mini AR transformer training in JAX....done")
+@click.command()
+@click.option("-r", "--run", default="single_batch")
+@click.option("-e", "--epochs", default=30)
+@click.option("-bs", "--batch_size", default=32)
+def main(run, epochs, batch_size):
+    embed_dim = 512
+    depth = 8
+
+    textdata = TextData()
+    train_loader = DataLoader(textdata, batch_size=batch_size, collate_fn=jax_collate)
+
+    vs = next(iter(train_loader))
+    print(f"sample token shape: {vs['input_ids'].shape}")
+
+    model = Transformer(depth, embed_dim, rngs=nnx.Rngs(config.seed))
+
+    testout = model(jnp.ones((1, 64, embed_dim)))
+    print(f"test output shape => {testout.shape}")
+
+    n_params = sum([p.size for p in jax.tree.leaves(nnx.state(model))])
+    print(f"model parameters count: {n_params/1e6:.2f}M, ")
+
+    optimizer = nnx.Optimizer(model, optax.adamw(learning_rate=config.learn_rate))
+
+    state = nnx.state((model, optimizer))
+    state = jax.device_put(state, jax.devices()[0])
+    nnx.update((model, optimizer), state)
+
+    sp = next(iter(train_loader))
+    print(f"loaded data \n data sample: {sp['vae_output'].shape}")
+
+    if run == "single_batch":
+        model, loss = batch_trainer(
+            epochs, model=model, optimizer=optimizer, train_loader=train_loader
+        )
+        wandb.finish()
+        print(f"single batch training ended at loss: {loss:.4f}")
+
+    elif run == "train":
+        print(f"you missed your train looop impl boy")
+
+
+main()
