c7807a56d18a47d496f4476ce9b938f77c91659a
Tensor <narutokelechi@gmail.com>
2025-01-25 04:26:47 +0100

    [tpu] update setup script

--- setup_tpu.sh
+++ setup_tpu.sh
@@ -1,21 +1,20 @@
-#until gcloud alpha compute tpus tpu-vm create trcnode-1 --project second-modem-441107-i1 --zone us-central2-b --accelerator-type v4-32 --version tpu-vm-base ; do : ; done
-
 ls /dev/accel*
 
 sudo apt-get update -y -qq
 sudo apt-get upgrade -y -qq
 sudo apt-get install -y -qq golang neofetch zsh byobu
-
+sudo snap install neofetch btop
 sudo apt-get install -y -qq software-properties-common
 sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo apt-get install -y -qq python3.12-full python3.12-dev
+sudo apt-get install -y -qq python3.12-full python3.12-dev python3.12-venv
 
-pip install -U pip jax-smi uv
-# python3.12 -m venv ~/venv
-uv venv .venv
+pip install -U pip
+mkdir playjax && cd playjax
+python3.12 -m venv ~/.venv
 source .venv/bin/activate
 
 pip install -U wheel
 pip install -U "jax[tpu]" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+pip install flax datasets numpy tqdm jax-smi wandb transformers
 
 python -c "import jax; print('testing tpus'); print(f'JAX recognized devices:\n local device count = {jax.local_device_count()} \n {jax.local_devices()}')"
